9b37127f9fed711acfcc6085501fc98bbfc49581
Ronaldyika <buhnyuyronald@gmail.com>
2023-08-04 15:51:02 +0100

    java user input using the java.util.scanner

--- array.java
+++ array.java
@@ -1,6 +1,27 @@
-class hello{
-    public static void main(String args[])
-    {
-        System.out.print("hello world\n");
-    }
+import java.util.Scanner;
+
+public class array{
+    public static void main(String[] args) {
+
+        int num1,num2;
+        num1 = 3;num2 = 3;
+
+        if(num1 > num2){
+            System.out.println("num1 is higher ");
+        }else if(num2 > num1){
+            System.out.println("num2 is higher");
+        }else{
+            System.out.println("the numbers are same ");
+        }
+
+        Scanner obj1 = new Scanner(System.in);
+
+        System.out.println("enter a name: \n");
+
+        String name = obj1.nextLine();
+
+        System.out.println("my name is " + name);
+
+
+}
 }
