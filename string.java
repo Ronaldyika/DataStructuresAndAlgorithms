5ce24f352979d525e46acfa5e7866dc79516e766
Ronaldyika <buhnyuyronald@gmail.com>
2023-08-04 16:12:18 +0100

    started with arrays

--- string.java
+++ string.java
@@ -1,4 +1,14 @@
+public class string{
 
-public class string {
+    public static void main(String[] args)
+    {
+        int [] numbers = new int[5];
 
+        System.out.println(numbers);
+
+        int [] num = {1,2,4,5,6};
+        System.out.println(num);
+        
+
+    }
 }
