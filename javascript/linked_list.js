cad89a53e7345aedcc485b8cc3f984a1ac7d173d
Ronaldyika <buhnyuyronald@gmail.com>
2023-09-30 08:02:33 +0100

    started linked list with classes

--- javascript/linked_list.js
+++ javascript/linked_list.js
@@ -22,7 +22,7 @@ nodeb.next = nodec;
 nodec.next = noded;
 
 console.log(nodea.next);
-================================*/
+
 const n1 = {
     data:200
 }
@@ -33,3 +33,48 @@ const n2 ={
 
 n1.next = n2;
 console.log(n1);
+================================*/
+
+
+class Node{
+    constructor(data,next = null){
+        this.data = data;
+        this.next = next;
+    }
+}
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    //insert first node
+    insertFirst(data){
+        this.head = new Node(data,this.next)
+    }
+
+    //insert last node
+
+    //insert at index
+
+    //get at index
+
+    //remove at index
+
+    //print list data
+}
+
+/*
+const n1 = new Node(200);
+const n2 = new Node(340,n1);
+const n3 = new Node(900,n2);
+const n4 = new Node(40,n3);
+console.log(n3);*/
+
+const ll = new LinkedList();
+ll.insertFirst(200);
+ll.insertFirst(470);
+
+console.log(ll);
+console.log(0.5 + 0.1 === 0.6)
+console.log(0.2 + 0.1 === 0.3)
