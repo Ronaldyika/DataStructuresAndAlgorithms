ac5599afa772404c1c167331955a3ea1b16e5b21
Ronaldyika <buhnyuyronald@gmail.com>
2023-11-14 00:49:38 +0100

    array

---
+++ OOP/array1.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class array1 {
+    public static void main(String[] args){
+
+        int [] array = new int[3];
+        array[0] = 3;
+        array[1] = 1;
+        System.out.println(Arrays.toString(array));
+    }
+    
+}
